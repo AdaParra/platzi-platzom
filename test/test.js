50789d0af826f13d0ed129bca11b7d41880e7444
Ada <paola.parra.sme@outlook.com>
2017-06-08 09:34:42 -0400

    Modificaciones

--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
 
 const expect = require('chai').expect
-const platzon = require('..').default
+const platzom = require('..').default
 
-describe('#platzon', function(){
+describe('#platzom', function(){
 	it('Si la palabra termina en "ar" se le quitan esos dos caracteres', function(){
-		const translation = platzon("Programar")
+		const translation = platzom("Programar")
 		expect(translation).to.equal("Program")
 	})
 	it('Si la palabra inicia con Z se le añade "pe" al final', function(){
-		const translation = platzon("Zorro")
-		const translation2 = platzon("Zarpar")
+		const translation = platzom("Zorro")
+		const translation2 = platzom("Zarpar")
 
 		expect(translation).to.equal("Zorrope")
 		expect(translation2).to.equal("Zarppe")
 	})
 	it('Si la palabra traducida tiene 10 o mas letras, se debe partir a la mitad y unir con un guion del medio', function(){
-		const translation = platzon("abecedario")
+		const translation = platzom("abecedario")
 		expect(translation).to.equal("abece-dario")
 	})
 	it('Si la palabra original es un palindromo, ninguna regla anterior cuenta y se devulve la misma palabra, intercalando Mayusculas y minusculas', function(){
-		const translation = platzon("sometemos")
+		const translation = platzom("sometemos")
 		expect(translation).to.equal("SoMeTeMoS")
 	})
 })
